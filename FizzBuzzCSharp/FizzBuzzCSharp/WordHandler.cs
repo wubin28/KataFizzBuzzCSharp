5b24cddef3455b97e266fc07ac067e5b9547295b
Ben Wu <wubin28@gmail.com>
2014-01-06 21:31:31 +0800

    Made all tests green.

--- FizzBuzzCSharp/FizzBuzzCSharp/WordHandler.cs
+++ FizzBuzzCSharp/FizzBuzzCSharp/WordHandler.cs
@@ -4,9 +4,13 @@
     {
         public string Handle(int number)
         {
-            if (number%3 == 0)
+            if (number % 3 == 0)
             {
                 return "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
             }
             return number.ToString();
         }
