cb732112b266e7f72dd325e7b58a9b3d01c2722f
Ben Wu <wubin28@gmail.com>
2014-01-06 21:15:34 +0800

    Added test WHEN_Three_SHOULD_SayFizz().

--- FizzBuzzCSharp/FizzBuzzCSharp/FizzBuzzTest.cs
+++ FizzBuzzCSharp/FizzBuzzCSharp/FizzBuzzTest.cs
@@ -15,5 +15,15 @@ namespace FizzBuzzCSharp
             // Then
             Assert.AreEqual("1", wordHandler.Handle(1), "Failure - when 1 should say 1");
         }
+
+        [Test]
+        public void WHEN_Three_SHOULD_SayFizz()
+        {
+            // Given
+            WordHandler wordHandler = new WordHandler();
+            // When
+            // Then
+            Assert.AreEqual("Fizz", wordHandler.Handle(3), "Failure - when 3 should say Fizz");
+        }
     }
 }
