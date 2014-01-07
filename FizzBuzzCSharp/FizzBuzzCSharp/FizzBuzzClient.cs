5e9b7b2ac945c79a64ecc022917de49c5dc63582
Ben Wu <wubin28@gmail.com>
2014-01-07 20:13:25 +0800

    Moved class WhizzHandler to another file.

--- FizzBuzzCSharp/FizzBuzzCSharp/FizzBuzzClient.cs
+++ FizzBuzzCSharp/FizzBuzzCSharp/FizzBuzzClient.cs
@@ -12,20 +12,4 @@
             return fizzBuzzHandler;
         }
     }
-
-    public class WhizzHandler : WordHandler
-    {
-        public WhizzHandler(WordHandler successor) : base (successor)
-        {
-        }
-
-        public override string Handle(int number)
-        {
-            if (number%7 == 0)
-            {
-                return "Whizz";
-            }
-            return successor.Handle(number);
-        }
-    }
 }
---
+++ FizzBuzzCSharp/FizzBuzzCSharp/WhizzHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace FizzBuzzCSharp
+{
+    public class WhizzHandler : WordHandler
+    {
+        public WhizzHandler(WordHandler successor) : base (successor)
+        {
+        }
+
+        public override string Handle(int number)
+        {
+            if (number%7 == 0)
+            {
+                return "Whizz";
+            }
+            return successor.Handle(number);
+        }
+    }
+}
