8e182595f8199c80c42f00db7fbec847fd8e6d37
Ben Wu <wubin28@gmail.com>
2014-01-07 19:41:59 +0800

    Created constructor of class WordHandler with a parameter as successor word handler.

--- FizzBuzzCSharp/FizzBuzzCSharp/WordHandler.cs
+++ FizzBuzzCSharp/FizzBuzzCSharp/WordHandler.cs
@@ -2,6 +2,13 @@
 {
     public abstract class WordHandler
     {
+        private WordHandler successor;
+
+        protected WordHandler(WordHandler successor)
+        {
+            this.successor = successor;
+        }
+
         public abstract string Handle(int number);
     }
 }
