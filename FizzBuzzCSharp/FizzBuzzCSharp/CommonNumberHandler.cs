03c29d5f5a637a980a61808b416d5163b179434b
Ben Wu <wubin28@gmail.com>
2014-01-07 19:40:29 +0800

    Created constructor of class CommonNumberHandler with a parameter as successor word handler.

--- FizzBuzzCSharp/FizzBuzzCSharp/CommonNumberHandler.cs
+++ FizzBuzzCSharp/FizzBuzzCSharp/CommonNumberHandler.cs
@@ -2,6 +2,10 @@
 {
     public class CommonNumberHandler : WordHandler
     {
+        public CommonNumberHandler(WordHandler successor) : base (successor)
+        {
+        }
+
         public override string Handle(int number)
         {
             return number.ToString();
