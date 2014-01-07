6f98234cc3e2250b6132429a2deefaa55b55eda4
Ben Wu <wubin28@gmail.com>
2014-01-07 19:50:42 +0800

    Fixed a typo in the class FizzBuzzClient.

--- FizzBuzzCSharp/FizzBuzzCSharp/FizzBuzzClient.cs
+++ FizzBuzzCSharp/FizzBuzzCSharp/FizzBuzzClient.cs
@@ -6,7 +6,7 @@
         {
             WordHandler commonNumberHandler = new CommonNumberHandler(null);
             WordHandler buzzHandler = new BuzzHandler(commonNumberHandler);
-            WordHandler fizzHandler = new FizzBuzzHandler(buzzHandler);
+            WordHandler fizzHandler = new FizzHandler(buzzHandler);
             WordHandler fizzBuzzHandler = new FizzBuzzHandler(fizzHandler);
             return fizzBuzzHandler;
         }
